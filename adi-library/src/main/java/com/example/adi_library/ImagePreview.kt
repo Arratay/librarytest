49b0f8cdf59cbcaacf5256053d67ac639c7ed1a3
Arratay <kacper.michalek00@gmail.com>
2024-06-13 15:58:23 +0200

    Initial commit

---
+++ adi-library/src/main/java/com/example/adi_library/ImagePreview.kt
@@ -0,0 +1,9 @@
+package com.example.adi_library
+
+import android.util.Log;
+
+object HelloWorld {
+    fun sayHello(): String {
+        return "Hello, World!"
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication
+
+import com.example.adi_library.HelloWorld
+
+class Main {
+    fun main() {
+        println(HelloWorld)
+    }
+}
